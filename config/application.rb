910732c93c20ee9ddf917ea19df43fb53d55710f
Jared Mull <jarmull@hgs.hiddengeniusproject.org>
2019-04-19 14:27:55 -0700

    t

--- config/application.rb
+++ config/application.rb
@@ -3,21 +3,21 @@ require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(*Rails.groups)
+#Bundler.require(*Rails.groups)
 
 module RailsSignupDownload
   class Application < Rails::Application
 
-    config.generators do |g|
-      g.test_framework :rspec,
-        fixtures: true,
-        view_specs: false,
-        helper_specs: false,
-        routing_specs: false,
-        controller_specs: false,
-        request_specs: false
-      g.fixture_replacement :factory_girl, dir: "spec/factories"
-    end
+    #config.generators do |g|
+      #g.test_framework :rspec,
+        # fixtures: true,
+        #view_specs: false,
+        #helper_specs: false,
+        #routing_specs: false,
+        #controller_specs: false,
+        #request_specs: false
+      #g.fixture_replacement :factory_girl, dir: "spec/factories"
+    #end
     config.assets.initialize_on_precompile = false
     config.assets.precompile += ['rails_admin/rails_admin.css', 'rails_admin/rails_admin.js']
 
