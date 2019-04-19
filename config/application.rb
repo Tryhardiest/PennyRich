6cff7fb96b8fa8b4ba93644c9de8221aa4000ab7
Jared Mull <jarmull@hgs.hiddengeniusproject.org>
2019-04-19 13:52:52 -0700

    l

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ Bundler.require(*Rails.groups)
 
 module RailsSignupDownload
   class Application < Rails::Application
-
+    config.assets.initialize_on_precompile = false
     config.generators do |g|
       g.test_framework :rspec,
         fixtures: true,
